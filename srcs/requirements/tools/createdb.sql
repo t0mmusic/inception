69530d8ff9c80cdf3a3e9248b32ce9c8e1c4c1b6
Jordan Brown <jebrown2091@gmail.com>
2023-02-27 10:18:23 +1030

    test mariadb setup

--- srcs/requirements/tools/createdb.sql
+++ srcs/requirements/tools/createdb.sql
@@ -1,4 +1,6 @@
-CREATE DATABASE WordPress CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON WordPress.* TO 'user'@'localhost';
+-- Creates a new database
+-- CREATE DATABASE WordPress CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+-- Creates user for database and grants all priviliges to user
+CREATE USER 'jbrown'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON WordPress.* TO 'jbrown'@'localhost';
 FLUSH PRIVILEGES;
